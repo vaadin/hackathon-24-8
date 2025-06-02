1911a02afc4577e8694c5b1fb8d1ad23db415e4f
start.vaadin.com <no-reply@vaadin.com>
2025-06-02 08:20:59 +0000

    Generated project

---
+++ src/test/java/net/flying/hooves/TestcontainersConfiguration.java
@@ -0,0 +1,10 @@
+package net.flying.hooves;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration {
+
+    // TODO Configure your Testcontainers here.
+    //  See https://docs.spring.io/spring-boot/reference/testing/testcontainers.html for details.
+}
