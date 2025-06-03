7fa00a253e021f58b70714115a6c3278b593ec11
abdullahtellioglu <abdullah@vaadin.com>
2025-06-03 10:02:27 +0300

    Application works and renders home view as expected

---
+++ src/main/frontend/views/task-list.tsx
@@ -0,0 +1,37 @@
+import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import { Button, DatePicker, Grid, GridColumn, TextField } from '@vaadin/react-components';
+import { Notification } from '@vaadin/react-components/Notification';
+import { useSignal } from '@vaadin/hilla-react-signals';
+import handleError from 'Frontend/views/_ErrorHandler';
+import { Group, ViewToolbar } from 'Frontend/components/ViewToolbar';
+import { useDataProvider } from '@vaadin/hilla-react-crud';
+
+export const config: ViewConfig = {
+  title: 'Task List',
+  menu: {
+    icon: 'vaadin:clipboard-check',
+    order: 1,
+    title: 'Task List',
+  },
+};
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: 'medium',
+  timeStyle: 'medium',
+});
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: 'medium',
+});
+
+type TaskEntryFormProps = {
+  onTaskCreated?: () => void;
+};
+
+
+export default function TaskListView() {
+
+  return (
+    <></>
+  );
+}
